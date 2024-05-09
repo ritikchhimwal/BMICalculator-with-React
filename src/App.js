8e37bb355e70900869e86e4737907cd9c007e3ae
ritikchhimwal <ritikchhimwal822@gmail.com>
2024-05-09 22:45:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
-import React, { useMemo, useState } from 'react'
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 const App = () => {
+  const [input ,setInput] = useState('');
+  const [amount, setAmount] = useState('');
+  const [expenses, setExpenses] = useState([]);
 
-  const [height,setHeight] = useState(180);
-  const [weight,setWeight] = useState(80);
+  const addExpense = () => {
+    if(!input || !amount) return;
 
-  function onWeightChange(event){
-    setWeight(event.target.value);
+    const newExpense ={
+      id: expenses.length+1,
+      title: input,
+      amount: amount
+    };
+    setExpenses([...expenses,newExpense]);
+    setInput('');
+    setAmount('');
   }
-
-  function onHeightChange(event){
-    setHeight(event.target.value);
+  const deleteExpense = (id) => {
+    setExpenses(expenses.filter((expenses) => expenses.id !== id))
   }
-
-  const output = useMemo(()=>{
-    const calculateHeight = height /100;
-
-    return(weight/(calculateHeight*calculateHeight)).toFixed(1)
-  },[weight,height])
-
-
   return (
     <main>
-      <h1>BMI Calculator</h1>
-      <div className='input-section'>
-        <p className='slider-output'>Weight:{weight}kg</p>
-        <input className='input-slider' type='range' step='1' min='40' max='200' onChange={onWeightChange}/>
-        <p className='slider-output'>Height:{height}cm</p>
-        <input className='input-slider' type='range' step='1' min='140' max='220' onChange={onHeightChange}/>
+      <h1>Expense Tracker</h1>
+      <div className='expense'>
+        <input type='text' placeholder='Expense' value={input} onChange={(e)=> setInput(e.target.value)}/>
+        <input type='number' placeholder='Amount' value={amount} onChange={(e) => setAmount(e.target.value)}/>
+        <button onClick={addExpense}>Add Expenses</button>
       </div>
-      <div className='output-section'><p>Your BMI is</p>
-      <p className='output'>{output}</p></div>
+      <ul className='expense-list'>
+        {expenses.map((exp)=>(
+          <li key={exp.id}>
+            <span>{exp.title}</span>
+            <span>{exp.amount}</span>
+            <button onClick={()=>deleteExpense(exp.id)}>Delete</button>
+          </li>
+        )) }
+      </ul>
     </main>
   )
 }
